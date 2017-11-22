b98aeebd92df06a6f5e5c7e5d73fdafc7ccf1d14
Sucan <qz2280@columbia.edu>
2017-11-22 12:43:11 -0500

    Compilable code

--- code/Scripts/distribute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-file=celq
-
-for folder in `ls -d -- */`
-do
-  cp $file $folder
-done
--- code/input3/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-for file in avec e eal
-do
-    gsed -i '1d' $file
-    gsed -i 's/^ //g' $file
-done
